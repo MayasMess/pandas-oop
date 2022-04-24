8bcaaa1f0aaddb009ee798d9f954757e7abc1e6e
messara <amayas.messara@gmail.com>
2022-04-24 02:16:07 +0200

    field refactor with a BaseColumn and add a base type :beers:

--- src/pandas_oop/fields.py
+++ src/pandas_oop/fields.py
@@ -5,63 +5,53 @@ import numpy as np
 from sqlalchemy import Column, Text, Integer, Float, Date, Boolean
 
 
-class StringColumn(pd.Series):
-    def __init__(self, **kwargs):
-        super().__init__(dtype='object')
-        self.str_type = "object"
-        self.np_type = np.str_
+class BaseColumn(pd.Series):
+    def __init__(self, base_type, dtype, np_type, **kwargs):
+        super().__init__(dtype=dtype)
+        self.str_type = dtype
+        self.np_type = np_type
+        self.base_type = base_type
         self.kwargs = copy(kwargs)
+
+    @staticmethod
+    def init_sqlalchemy_column(sqlalchemy_col_type, **kwargs):
         kwargs['primary_key'] = kwargs.pop('unique', None)
         kwargs.pop('target_name', None)
-        self.sqlalchemy_column = Column(Text, **kwargs)
+        return Column(sqlalchemy_col_type, **kwargs)
 
 
-class IntegerColumn(pd.Series):
+class StringColumn(BaseColumn):
     def __init__(self, **kwargs):
-        super().__init__(dtype='int64')
-        self.str_type = "int64"
-        self.np_type = np.int64
-        self.kwargs = copy(kwargs)
-        kwargs['primary_key'] = kwargs.pop('unique', None)
-        kwargs.pop('target_name', None)
-        self.sqlalchemy_column = Column(Integer, **kwargs)
+        super().__init__(base_type='object', dtype='object', np_type=np.str_, **kwargs)
+        self.sqlalchemy_column = self.init_sqlalchemy_column(Text, **kwargs)
 
 
-class FloatColumn(pd.Series):
+class IntegerColumn(BaseColumn):
     def __init__(self, **kwargs):
-        super().__init__(dtype='float64')
-        self.str_type = "float64"
-        self.np_type = np.float64
-        self.kwargs = copy(kwargs)
-        kwargs['primary_key'] = kwargs.pop('unique', None)
-        kwargs.pop('target_name', None)
-        self.sqlalchemy_column = Column(Float, **kwargs)
+        super().__init__(base_type='int', dtype='int64', np_type=np.int64, **kwargs)
+        self.sqlalchemy_column = self.init_sqlalchemy_column(Integer, **kwargs)
 
 
-class DateColumn(pd.Series):
+class FloatColumn(BaseColumn):
     def __init__(self, **kwargs):
-        super().__init__(dtype='datetime64[ns]')
-        self.str_type = 'datetime64[ns]'
-        self.np_type = np.datetime64
-        self.kwargs = copy(kwargs)
+        super().__init__(base_type='float', dtype='float64', np_type=np.float64, **kwargs)
+        self.sqlalchemy_column = self.init_sqlalchemy_column(Float, **kwargs)
+
+
+class DateColumn(BaseColumn):
+    def __init__(self, **kwargs):
+        super().__init__(base_type='datetime', dtype='datetime64[ns]', np_type=np.datetime64, **kwargs)
         if kwargs.get('format') is not None:
             del kwargs['format']
-        kwargs['primary_key'] = kwargs.pop('unique', None)
-        kwargs.pop('target_name', None)
-        self.sqlalchemy_column = Column(Date, **kwargs)
+        self.sqlalchemy_column = self.init_sqlalchemy_column(Date, **kwargs)
 
 
-class BoolColumn(pd.Series):
+class BoolColumn(BaseColumn):
     def __init__(self, **kwargs):
-        super().__init__(dtype='bool')
-        self.str_type = "bool"
-        self.np_type = np.bool_
+        super().__init__(base_type='bool', dtype='bool', np_type=np.bool_, **kwargs)
         self.true_or_false = None
-        self.kwargs = copy(kwargs)
         if kwargs.get('true') is not None and kwargs.get('false') is not None:
             self.true_or_false = {kwargs.get('true'): True, kwargs.get('false'): False}
             del kwargs['true']
             del kwargs['false']
-        kwargs['primary_key'] = kwargs.pop('unique', None)
-        kwargs.pop('target_name', None)
-        self.sqlalchemy_column = Column(Boolean, **kwargs)
+        self.sqlalchemy_column = self.init_sqlalchemy_column(Boolean, **kwargs)
